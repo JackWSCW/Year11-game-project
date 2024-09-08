07327c6e604f86df1488ec0a73df4a9e3734421a
JackWSCW <159732885+JackWSCW@users.noreply.github.com>
2024-09-09 11:57:40 +1200

    log jump challenge, started kill zone

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 120.0
-const JUMP_VELOCITY = -225.0
+const JUMP_VELOCITY = -175.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
